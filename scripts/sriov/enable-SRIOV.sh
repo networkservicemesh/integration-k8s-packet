f8a770af518251a2db32f61fce49660b5f5852b8
Vladimir Popov <vladimir.popov@xored.com>
2021-05-07 18:21:03 +0700

    Use fix for the boot issue
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- scripts/sriov/enable-SRIOV.sh
+++ scripts/sriov/enable-SRIOV.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# https://gist.github.com/vielmetti/dafb5128ef7535c218f6d963c5bc624e
+apt-get update
+apt-get install -y grub2-common
+grub-install --bootloader-id=ubuntu
+
 sed -Ei "s/(GRUB_CMDLINE_LINUX=.*)'/\1 intel_iommu=on'/" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 

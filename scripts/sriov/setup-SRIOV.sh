f0174bc152e4633025bb28516cbca409daa4015a
Vladimir Popov <vladimir.popov@xored.com>
2021-10-28 18:58:08 +0700

    Add scripts for Calico CNI setup
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- scripts/sriov/setup-SRIOV.sh
+++ scripts/sriov/setup-SRIOV.sh
@@ -5,22 +5,12 @@ master_ip="$1"
 worker_ip="$2"
 SSH_OPTS="$3"
 
-function wait_pids() {
-  pids="$1"
-  message="$2"
-  for pid in ${pids}; do
-    echo "waiting for PID ${pid}"
-    wait ${pid}
-    code=$?
-    if test $code -ne 0; then
-      echo "${message}: process exited with code $code, aborting..."
-      return 1
-    fi
-  done
-  return 0
-}
-
 SRIOV_DIR=$(dirname "$0")
+
+# wait_pids pid_1 ... pid_n
+source scripts/include/wait-pids.sh
+# wait_start ip_1 ... ip_n
+source scripts/include/wait-start.sh
 
 # Create SR-IOV scripts directory on nodes
 ssh ${SSH_OPTS} root@${master_ip} mkdir sriov
@@ -37,30 +27,7 @@ ssh ${SSH_OPTS} root@${worker_ip} ./sriov/enable-SRIOV.sh &
 pids+=" $!"
 wait_pids "${pids}" "SR-IOV setup failed" || exit 3
 
-sleep 5
-
-for ip in ${master_ip} ${worker_ip}; do
-  success_attempts=0
-  # ~15 minutes to start
-  for i in {1..60}; do
-    if [[ ${i} == 60 ]]; then
-      echo "timeout waiting for the ${ip} to start, aborting..."
-      exit 4
-    fi
-
-    if ssh ${SSH_OPTS} -o ConnectTimeout=1 -o BatchMode=yes root@${ip} true; then
-      ((success_attempts++))
-    else
-      success_attempts=0
-    fi
-
-    if [[ ${success_attempts} == 3 ]]; then
-      break
-    fi
-
-    sleep 15
-  done
-done
+wait_start ${master_ip} ${worker_ip}
 
 # Create SR-IOV config
 scp ${SSH_OPTS} ${SRIOV_DIR}/config-SRIOV.sh root@${master_ip}:sriov/config-SRIOV.sh || exit 5

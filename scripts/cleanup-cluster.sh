50433930da5889013526fc50691e13502604e31b
Artem Glazychev <artem.glazychev@xored.com>
2023-02-20 19:33:20 +0700

    Update packet setup
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- scripts/cleanup-cluster.sh
+++ scripts/cleanup-cluster.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -x
 
-ssh_id=$1
-
 ## Cluster API Packet provides, together with the cluster, always creates an address that must be manually deleted
 
-yes | metal ssh-key delete --id "$ssh_id"
 eip=$(metal ip get -p "${PROJECT_ID}" -o json | jq -r '.[] | select( .tags != null and any(.tags[]; endswith(env.CLUSTER_NAME))) | .id')
 metal ip remove -i "$eip"
 kubectl delete cluster "${CLUSTER_NAME}"
